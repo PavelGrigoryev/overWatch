f5d8ac799054aa722511b162d452c5521f1a0f28
Grigoryev Pavel <Green-ono@yandex.ru>
2022-11-07 17:25:54 +0300

    added swagger openapi

--- src/main/java/by/grigoryev/overwatch/controller/CoinController.java
+++ src/main/java/by/grigoryev/overwatch/controller/CoinController.java
@@ -2,6 +2,9 @@ package by.grigoryev.overwatch.controller;
 
 import by.grigoryev.overwatch.dto.CoinDto;
 import by.grigoryev.overwatch.service.CoinService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +17,25 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/coins")
+@Tag(name = "Coin", description = "The Coin API")
 public class CoinController {
 
     private final CoinService coinService;
 
+    @Operation(
+            summary = "View a list of available cryptocurrencies.", tags = "Coin",
+            description = "View a list of available cryptocurrencies."
+    )
     @GetMapping
     public Flux<CoinDto> viewListOfAvailable() {
         return coinService.viewListOfAvailable();
     }
 
+    @Operation(
+            summary = "View the current price for the specified cryptocurrency.", tags = "Coin",
+            description = "Let's find our cryptocurrency by symbol",
+            parameters = @Parameter(name = "symbol", description = "Enter symbol here", example = "BTC")
+    )
     @GetMapping("/{symbol}")
     public Mono<ResponseEntity<CoinDto>> findFirstBySymbolOrderByTimeOfReceivingDesc(@PathVariable String symbol) {
         return coinService.findFirstBySymbolOrderByTimeOfReceivingDesc(symbol)
