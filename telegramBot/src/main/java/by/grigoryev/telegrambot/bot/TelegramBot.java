e7334926c26f4dc03c53ce98d31b57e6f8fa754f
Grigoryev Pavel <Green-ono@yandex.ru>
2022-11-14 22:23:49 +0300

    delete localization

--- telegramBot/src/main/java/by/grigoryev/telegrambot/bot/TelegramBot.java
+++ telegramBot/src/main/java/by/grigoryev/telegrambot/bot/TelegramBot.java
@@ -49,56 +49,29 @@ public class TelegramBot extends TelegramLongPollingBot {
         log.warn(user.getUserName() + " " + user.getFirstName() + " " + user.getLastName() + " wrote: " + text);
 
         if (message.hasText()) {
-            if (user.getLanguageCode().equals("ru")) {
-                sendText(user.getId(), "Дорогой " + user.getFirstName() +
-                        " \uD83D\uDC49\uD83D\uDC4C\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6");
+            sendText(user.getId(), "Dear " + user.getFirstName() +
+                    " \uD83D\uDC49\uD83D\uDC4C\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6");
 
-                switch (text) {
-                    case "/notifybtc", "/notifyeth", "/notifysol" -> telegramUserService
-                            .register(text.substring(7, 10).toUpperCase(), update)
-                            .subscribe(telegramUserDto -> sendText(user.getId(),
-                                    "Вы будете уведомлены если цена крипты "
-                                            + text.substring(7, 10).toUpperCase() + " изменится"));
-                    case "/viewall" -> telegramUserService.viewListOfAvailable()
-                            .subscribe(showTelegramCoinDtoToUser(user, formatter));
-                    case "/btc", "/eth", "/sol" ->
-                            telegramUserService.findFirstBySymbol(text.substring(1, 4).toUpperCase())
-                                    .subscribe(showTelegramCoinDtoToUser(user, formatter));
-                    default -> sendText(user.getId(), """
-                            Доступные команды :
-                            /notifybtc - уведомляет юзера о изменении цены 0.01% на Биткоин
-                            /notifyeth - уведомляет юзера о изменении цены 0.01% на Эфир
-                            /notifysol - уведомляет юзера о изменении цены 0.01% на Солану
-                            /viewall - показывает актуальную цену на все крипто валюты
-                            /btc - показывает актуальную цену на Биткоин
-                            /eth - показывает актуальную цену на Эфир
-                            /sol - показывает актуальную цену на Солану""");
-                }
-            } else {
-                sendText(user.getId(), "Dear " + user.getFirstName() +
-                        " \uD83D\uDC49\uD83D\uDC4C\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6\uD83D\uDCA6");
+            switch (text) {
+                case "/notifybtc", "/notifyeth", "/notifysol" -> telegramUserService
+                        .register(text.substring(7, 10).toUpperCase(), update)
+                        .subscribe(telegramUserDto -> sendText(user.getId(),
+                                "You will be notified if the price of cryptocurrency "
+                                        + text.substring(7, 10).toUpperCase() + " changes"));
+                case "/viewall" -> telegramUserService.viewListOfAvailable()
+                        .subscribe(showTelegramCoinDtoToUser(user, formatter));
+                case "/btc", "/eth", "/sol" -> telegramUserService.findFirstBySymbol(text.substring(1, 4).toUpperCase())
+                        .subscribe(showTelegramCoinDtoToUser(user, formatter));
+                default -> sendText(user.getId(), """
+                        Available commands :
+                        /notifybtc - notifies the user about a change 0.01% in Bitcoin
+                        /notifyeth - notifies the user about a change 0.01% in Etherium
+                        /notifysol - notifies the user about a change 0.01% in Solana
+                        /viewall - shows the current price at the moment for all
+                        /btc - shows the current price at the moment for Bitcoin
+                        /eth - shows the current price at the moment for Etherium
+                        /sol - shows the current price at the moment for Solana""");
 
-                switch (text) {
-                    case "/notifybtc", "/notifyeth", "/notifysol" -> telegramUserService
-                            .register(text.substring(7, 10).toUpperCase(), update)
-                            .subscribe(telegramUserDto -> sendText(user.getId(),
-                                    "You will be notified if the price of cryptocurrency "
-                                            + text.substring(7, 10).toUpperCase() + " changes"));
-                    case "/viewall" -> telegramUserService.viewListOfAvailable()
-                            .subscribe(showTelegramCoinDtoToUser(user, formatter));
-                    case "/btc", "/eth", "/sol" ->
-                            telegramUserService.findFirstBySymbol(text.substring(1, 4).toUpperCase())
-                                    .subscribe(showTelegramCoinDtoToUser(user, formatter));
-                    default -> sendText(user.getId(), """
-                            Available commands :
-                            /notifybtc - notifies the user about a change 0.01% in Bitcoin
-                            /notifyeth - notifies the user about a change 0.01% in Etherium
-                            /notifysol - notifies the user about a change 0.01% in Solana
-                            /viewall - shows the current price at the moment for all
-                            /btc - shows the current price at the moment for Bitcoin
-                            /eth - shows the current price at the moment for Etherium
-                            /sol - shows the current price at the moment for Solana""");
-                }
             }
         } else {
             sendText(user.getId(), "Available only text messages!");
